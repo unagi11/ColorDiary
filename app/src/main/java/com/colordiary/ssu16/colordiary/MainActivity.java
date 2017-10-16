5c6f4c62722f15cb8e430de394175ea4280a0869
Unagi11 <unagi11@naver.com>
2017-10-16 21:44:43 +0900

    notebook commit

--- app/src/main/java/com/colordiary/ssu16/colordiary/MainActivity.java
+++ app/src/main/java/com/colordiary/ssu16/colordiary/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //수정2
+        //새로운 컴퓨터에서 issue01 수정
     }
 }
