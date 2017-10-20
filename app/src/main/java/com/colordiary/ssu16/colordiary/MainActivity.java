e3ee7b2101b87ff53592b7c46e2cbac59b12e043
seungy97 <32810013+seungy97@users.noreply.github.com>
2017-10-20 15:15:28 +0900

    new branch

--- app/src/main/java/com/colordiary/ssu16/colordiary/MainActivity.java
+++ app/src/main/java/com/colordiary/ssu16/colordiary/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //수정2
-        //new 
+        //new
     }
 }
