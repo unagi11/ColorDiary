b0b84d3829b75a3b0084fc4ba7458a53fc19984c
seungy97 <32810013+seungy97@users.noreply.github.com>
2017-10-20 15:20:18 +0900

    작업다함

--- app/src/main/java/com/colordiary/ssu16/colordiary/MainActivity.java
+++ app/src/main/java/com/colordiary/ssu16/colordiary/MainActivity.java
@@ -9,7 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //수정2
-        //new
+        //작업 다했다.
     }
 }
