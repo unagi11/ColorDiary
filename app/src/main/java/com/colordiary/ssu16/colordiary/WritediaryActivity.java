b234736245dd7228e9a129ea33429ca9edf4d3af
Unagi11 <unagi11@naver.com>
2017-11-19 18:46:16 +0900

    변경점 - 사진 불러오기 추가

--- app/src/main/java/com/colordiary/ssu16/colordiary/WritediaryActivity.java
+++ app/src/main/java/com/colordiary/ssu16/colordiary/WritediaryActivity.java
@@ -1,26 +1,75 @@
 package com.colordiary.ssu16.colordiary;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class WritediaryActivity extends AppCompatActivity {
+
+    private static final int PICK_IMAGE_REQUEST = 1;
+    EditText editText_Title, editText_Diary;
+    ImageButton imageButton_Diary;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_writediary);
-
-//        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xFF339999));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        editText_Title = (EditText)findViewById(R.id.TitleEditText);
+        editText_Diary = (EditText)findViewById(R.id.DiaryEditText);
+        imageButton_Diary = (ImageButton)findViewById(R.id.DiaryImage);
+        imageButton_Diary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT); //ACTION_PIC과 차이점?
+                intent.setType("image/*"); //이미지만 보이게
+                //Intent 시작 - 갤러리앱을 열어서 원하는 이미지를 선택할 수 있다.
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+            }
+        });
     }
 
-    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            //이미지를 하나 골랐을때
+            if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && null != data) {
+                //data에서 절대경로로 이미지를 가져옴
+                Uri uri = data.getData();
+
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
+                //이미지가 한계이상(?) 크면 불러 오지 못하므로 사이즈를 줄여 준다.
+                int nh = (int) (bitmap.getHeight() * (1024.0 / bitmap.getWidth()));
+                Bitmap scaled = Bitmap.createScaledBitmap(bitmap, 1024, nh, true);
+                imageButton_Diary.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageButton_Diary.setImageBitmap(scaled);
+            } else {
+                Toast.makeText(this, "사진 불러오기 취소 되었습니다.", Toast.LENGTH_LONG).show();
+            }
+
+        } catch (Exception e) {
+            Toast.makeText(this, "Oops! 로딩에 오류가 있습니다.", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+
+    }
+
+@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return super.onCreateOptionsMenu(menu);
