a39d90c550af18da423da7d0628e1b0faea75d0e
soohee <su0766@gmail.com>
2017-11-24 14:16:50 +0900

    comment

--- app/src/main/java/com/colordiary/ssu16/colordiary/MainActivity.java
+++ app/src/main/java/com/colordiary/ssu16/colordiary/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //새로운 컴퓨터에서 issue01 수정
-        //작업 다했다.
+
     }
 }
+//branch issue#5로 만듬!
